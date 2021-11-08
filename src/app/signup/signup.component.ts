b6a43c0cbc374525670efe60dd33e10a5f618b1f
Amanda <amandaamorim.ctt@gmail.com>
2021-11-08 01:46:29 -0300

    [@amandioca - Task: ADD method register in TS, and other methods] 2021.11.08

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,49 @@
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from './../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model/Usuario';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario();
+  confirm: string;
+  type: string;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    window.scroll(0, 0);
+  }
+
+  confirmPassword(event: any) {
+    this.confirm = event.target.value;
+  }
+
+  userType(event: any) {
+    this.type = event.target.value;
+  }
+
+  register() {
+    this.usuario.tipo = this.type
+
+    if(this.usuario.senha != this.confirm) {
+      alert('Passwords do not match. Try one more time')
+
+    } else{
+      this.authService.signup(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp
+        this.router.navigate(['/login'])
+        alert('Successfully registered user')
+      })
+    }
   }
 
 }
