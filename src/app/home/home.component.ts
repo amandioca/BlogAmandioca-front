3d32937f843c58c86950281b329179d041ebe6e2
Amanda <amandaamorim.ctt@gmail.com>
2021-11-08 11:53:49 -0300

    [@amandioca - Task: ADD environments] 2021.11.08

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  name = environment.name
+
   constructor(
     private router: Router
   ) { }
