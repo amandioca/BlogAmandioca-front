fe6a4ba68c0466e63fd9a964c58a4d67ffcf4ebc
Amanda <amandaamorim.ctt@gmail.com>
2021-11-10 19:55:24 -0300

    [@amandioca - Task: CREATE component theme] 2021.11.09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { HomeComponent } from './home/home.component';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { ThemeComponent } from './theme/theme.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     FooterComponent,
     LoginComponent,
     SignupComponent,
-    HomeComponent
+    HomeComponent,
+    ThemeComponent,
   ],
   imports: [
     BrowserModule,
