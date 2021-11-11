9c9146949b457a75c8d1acc66e54da591016ef24
Amanda <amandaamorim.ctt@gmail.com>
2021-11-11 16:09:18 -0300

    [@amandioca - Task: ADD token and methods get and put] 2021.11.11

--- src/app/service/theme.service.ts
+++ src/app/service/theme.service.ts
@@ -1,9 +1,29 @@
+import { Tema } from './../model/Tema';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Tema } from '../model/Tema';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+    token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+
+    getAllTheme(): Observable<Tema[]>{
+      return this.http.get<Tema[]>('https://amandioca.herokuapp.com/temas')
+    }
+
+    postTheme(tema: Tema): Observable<Tema>{
+      return this.http.post<Tema>('https://amandioca.herokuapp.com/temas', tema, this.token)
+    }
+
 }
